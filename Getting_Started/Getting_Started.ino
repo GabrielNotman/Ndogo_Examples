e11ff753a9ed83fb3a7f5059da548ee974c0bcaf
Gabriel Notman <g.notman@gmail.com>
2015-04-16 10:58:17 +0100

    Minor comment change.

--- Getting_Started/Getting_Started.ino
+++ Getting_Started/Getting_Started.ino
@@ -1,13 +1,13 @@
 void setup() 
 {
-  // put your setup code here, to run once:
+  //Put your setup code here, to run once
   Serial.begin(9600);
   Serial.println("Starting...");
 }
 
 void loop() 
 {
-  // put your main code here, to run repeatedly:
+  //Put your main code here, to run repeatedly
   Serial.println("Hello World");
   delay(1000);
 }
